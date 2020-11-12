d5b97b1d8101d75393a0f2c0ee12b959075b648d
hubiqing <hbqhbq123@qq.com>
2020-11-12 16:52:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="test">test</button>
+</template>
+<script>
+import importHTML from 'import-html-entry'
+export default {
+  name: 'App',
+  data() {
+    return {
+      x: '1',
+    }
+  },
+  methods: {
+    test() {
+      importHTML('demo.html').then((res) => {
+        debugger
+        console.log(res.template)
+
+        res.execScripts().then((exports) => {
+          const mobx = exports
+          const { observable } = mobx
+          observable({
+            name: 'kuitos',
+          })
+        })
+      })
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
